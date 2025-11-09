991090b692f5c9859ed87839723cbfe2c9194dae
chrismalone617 <chris@m11design.com>
2025-11-09 12:52:03 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -464,7 +464,7 @@ export const onRequestGet = async ({ request, env }) => {
     if(d.length===11 && d[0]==='1') d = d.slice(1);
     if(d.length!==10) return { tel:null, display: raw||'' };
     const tel = '+1'+d;
-    const display = \`(\${d.slice(0,3)}) \${d.slice(3,6)}-\${d.slice(6)}\`;
+    const display = '(' + d.slice(0,3) + ') ' + d.slice(3,6) + '-' + d.slice(6);
     return { tel, display };
   }
 };
