b05211876b0e395a8ffca7bbfd4ff3397802bd4b
chrismalone617 <chris@m11design.com>
2025-11-25 07:49:29 -0500

    Change page title layout

--- functions/index.js
+++ functions/index.js
@@ -592,8 +592,7 @@ export const onRequestGet = async ({ request, env }) => {
     <div class="container py-3">
       <div class="flex flex-col gap-2 md:flex-row items-center md:items-center md:justify-between">
         <div>
-          <h1 class="text-2xl font-bold">${escapeHtml(page_title || 'Directory')}</h1>
-                  </div>
+<h1 class="text-2xl font-bold whitespace-pre-line">${escapeHtml(page_title || 'Directory')}</h1>                  </div>
         <div class="flex gap-2 items-center filters-row">
           <input id="q" class="srch border rounded-lg px-3 py-2" type="search" placeholder="Search this page">
           <select id="cat" class="border rounded-lg px-2 py-2">
