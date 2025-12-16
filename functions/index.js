dd4a3f4b8a55e762b532272ddb30b54744febb4e
Chris Malone <chris@m11design.com>
2025-12-16 14:22:27 -0500

    changed the twitter logo to the X

--- functions/index.js
+++ functions/index.js
@@ -1003,7 +1003,7 @@ Business Owners - would you like to appear on one of our directory pages? We off
             </a>
             <a href="https://x.com/mineralforum" aria-label="X (Twitter)" target="_blank">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
+                <path d="M18 6L6 18M6 6l12 12"></path>
               </svg>
             </a>
             <a href="https://www.linkedin.com/company/the-mineral-rights-forum" aria-label="LinkedIn" target="_blank">
